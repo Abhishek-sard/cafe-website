5bf7650e8747435d162582dbc56b7fff5cbefee3
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-11-27 12:32:25 +0545

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,6 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <div className="text-center mt-10 text-white">
-        <h1 className="text-3xl text-yellow-400 font-bold">Elite Café Navigation</h1>
-        <p className="text-gray-300 mt-4">
-          This is the Tailwind CSS version of your café navigation bar.
-        </p>
-      </div>
     </>
   );
 };
---
+++ client/src/Components/DeliveryTracking.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DeliveryTracking = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DeliveryTracking
